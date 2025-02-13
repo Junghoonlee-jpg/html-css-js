07cb6c6437cbe59c1601c51dde1a414058774a73
Junghoonlee-jpg <ljh2ljh2@naver.com>
2025-02-13 17:22:00 +0900

    day2 finished

--- third/index.js
+++ third/index.js
@@ -1,17 +1,14 @@
 document.write("Ready to go");
-
-const 막내 = document.querySelector("body > div > div > div:last-child");
-
-// 막내.style.color = "tomato";
-
+// const 막내 = document.querySelector("body > div > div > div:last-child")
+// querySelector = css 규칙 그대로 적용됨
+// 막내.style.color = "tomato"
 const id = document.querySelector("#find_id");
 const pw = document.querySelector("#find_pw");
 const sign = document.querySelector("#sign_up");
-
 id.onclick = () => {
   alert("아이디를 찾으시겠습니까?");
-  id.style.color = "black";
-  pw.style.color = "blue";
+  id.style.color = "blue";
+  pw.style.color = "black";
   sign.style.color = "black";
 };
 pw.onclick = () => {
@@ -23,6 +20,6 @@ pw.onclick = () => {
 sign.onclick = () => {
   alert("회원가입 하시겠습니까?");
   id.style.color = "black";
-  pw.style.color = "blue";
-  sign.style.color = "black";
+  pw.style.color = "black";
+  sign.style.color = "blue";
 };
